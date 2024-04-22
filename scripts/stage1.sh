556d2561f72f10b14cbd80a96a26a748492dcd21
Vitaliy Alifanov <alifanov.vitalik@yandex.ru>
2024-04-22 21:39:02 +0300

    add avro and snappy

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -1,2 +1,8 @@
-#!/bin/bash
+# Import to PSQL
 python scripts/build_projectdb.py
+
+# TODO: add password secret
+password=pytttPLjPeOI7kc1
+
+# Transfer data from PSQL to HDFS
+sqoop import-all-tables --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team15_projectdb --username team15 --password $password --compression-codec=snappy --compress --as-avrodatafile --warehouse-dir=project/warehouse --m 1
