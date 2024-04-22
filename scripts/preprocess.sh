27f6e2107ef2a4b7befcfb130314f1931d6f1e63
Daniel Vakhrushev <45727078+danielpancake@users.noreply.github.com>
2024-04-22 20:18:33 +0300

    HOT PATHS FIX

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -22,4 +22,4 @@ unzip data/usedcars.zip -d data/raw/
 rm data/usedcars.zip
 
 # Run data preprocessing
-python ./scripts/data-preprocessing.py 'data/raw/vehicles.csv' --output 'data/processed/sliced.csv'
+python scripts/data_preprocessing.py 'data/raw/vehicles.csv' --output 'data/processed/sliced.csv'
