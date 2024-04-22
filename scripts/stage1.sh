7294ee1e2e0100e0fd0a537ad23355156bb30704
Vitaliy Alifanov <alifanov.vitalik@yandex.ru>
2024-04-22 21:58:46 +0300

    automate import to HDFS

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -4,7 +4,8 @@ python scripts/build_projectdb.py
 # TODO: add password secret
 password=pytttPLjPeOI7kc1
 
-# TODO: clear HDFS warehouse folder before transfer
+# Clear HDFS warehouse folder before transfer
+sqoop import --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team15_projectdb --username team15 --password $password --table car_description --delete-target-dir --target-dir 'project/warehouse/' -m 1
 
 # Transfer data from PSQL to HDFS
 sqoop import-all-tables --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team15_projectdb --username team15 --password $password --compression-codec=snappy --compress --as-avrodatafile --warehouse-dir=project/warehouse --m 1
