c7d5b9df9f8abcfbcaf550b986dfefce0593a301
Daniel Vakhrushev <45727078+danielpancake@users.noreply.github.com>
2024-04-22 19:50:46 +0300

    Pylint fixes

--- scripts/build_projectdb.py
+++ scripts/build_projectdb.py
@@ -1,6 +1,10 @@
-import psycopg2 as psql
-from pprint import pprint
+""" Connects to dataset, creates tables and imports data """
+
 import os
+
+from pprint import pprint
+
+import psycopg2 as psql
 
 
 # Read password from secrets file
@@ -9,26 +13,36 @@ import os
 #     password = file.read().rstrip()
 
 # build connection string
-conn_string = "host=hadoop-04.uni.innopolis.ru port=5432 user=team15 dbname=team15_projectdb password={}".format(
-    'pytttPLjPeOI7kc1')
+HOST = "hadoop-04.uni.innopolis.ru"
+PORT = "5432"
+USER = "team15"
+DBNAME = "team15_projectdb"
+
+PSWD = "pytttPLjPeOI7kc1"  # TODO: import from file
+
+conn_string = f"host={HOST} port={PORT} user={USER} dbname={DBNAME} password={PSWD}"
 
 
 # Connect to the remote dbms
 with psql.connect(conn_string) as conn:
-
     # Create a cursor for executing psql commands
     cur = conn.cursor()
+
     # Read the commands from the file and execute them.
-    with open(os.path.join("sql", "create_tables.sql")) as file:
+    with open(os.path.join("sql", "create_tables.sql"), encoding="utf-8") as file:
         content = file.read()
         cur.execute(content)
+
     conn.commit()
 
     # Read the commands from the file and execute them.
-    with open(os.path.join("sql", "import_data.sql")) as file:
+    with open(os.path.join("sql", "import_data.sql"), encoding="utf-8") as file:
         # We assume that the COPY commands in the file are ordered (1.depts, 2.emps)
         commands = file.readlines()
-        with open(os.path.join("data", "raw\vehicles.csv"), "r") as vehicle:
+
+        with open(
+            os.path.join("data", "raw/vehicles.csv"), "r", encoding="utf-8"
+        ) as vehicle:
             cur.copy_expert(commands[0], vehicle)
 
     # If the sql statements are CRUD then you need to commit the change
@@ -36,10 +50,11 @@ with psql.connect(conn_string) as conn:
 
     pprint(conn)
     cur = conn.cursor()
+
     # Read the sql commands from the file
-    with open(os.path.join("sql", "test_database.sql")) as file:
+    with open(os.path.join("sql", "test_database.sql"), encoding="utf-8") as file:
         commands = file.readlines()
+
         for command in commands:
             cur.execute(command)
-            # Read all records and print them
-            pprint(cur.fetchall())
+            pprint(cur.fetchall())  # Read all records and print them
