febd25ce1a4acff4d8035febfa394fddfe8392d3
Vitaliy Alifanov <alifanov.vitalik@yandex.ru>
2024-04-22 21:42:01 +0300

    add todo in stage1.sh

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -4,5 +4,7 @@ python scripts/build_projectdb.py
 # TODO: add password secret
 password=pytttPLjPeOI7kc1
 
+# TODO: clear HDFS warehouse folder before transfer
+
 # Transfer data from PSQL to HDFS
 sqoop import-all-tables --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team15_projectdb --username team15 --password $password --compression-codec=snappy --compress --as-avrodatafile --warehouse-dir=project/warehouse --m 1
