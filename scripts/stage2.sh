9d077fbab4128ad1b907d2a0e25adfc6338bb029
Vitaliy Alifanov <alifanov.vitalik@yandex.ru>
2024-04-24 21:13:07 +0300

    create hive tables

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -4,3 +4,5 @@ source secrets/.psql.pass
 beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f sql/db.hql > output/hive_results.txt
 
 beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f sql/create_partitioned_tables.hql
+
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f sql/q1.hql > output/q1.txt
