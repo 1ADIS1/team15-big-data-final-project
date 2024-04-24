3e2537103af6e87a1c7117b427af522b9b1395d1
Daniel Vakhrushev <45727078+danielpancake@users.noreply.github.com>
2024-04-24 20:56:36 +0300

    Forgor we're not 3.11

--- scripts/build_projectdb.py
+++ scripts/build_projectdb.py
@@ -1,7 +1,7 @@
 """ Connects to dataset, creates tables and imports data """
 
 import os
-import tomllib
+import toml
 
 from pprint import pprint
 
@@ -17,8 +17,8 @@ DBNAME = "team15_projectdb"
 # Read password from secrets file
 file = os.path.join("secrets", ".psql.pass")
 
-with open(file, "rb") as f:
-    secrets = tomllib.load(f)
+with open(file, "r", encoding="utf-8") as f:
+    secrets = toml.load(f)
 
 CONN_STRING = f"host={HOST} port={PORT} user={USER} dbname={DBNAME} password={secrets.get('password', 'admin')}"
 
