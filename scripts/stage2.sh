0e96af48795cb357f8f9f634d64a3438cfbd0811
Ivan Chernakov <ivancholeg@gmail.com>
2024-04-29 20:30:58 +0300

    Update stage2.sh

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -15,7 +15,7 @@ beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password 
 # Add a header to the output file
 echo "US_state,total_vehicles" > output/q1.csv 
 
-beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n teamx -p $password -f sql/q1.hql
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f sql/q1.hql
 
 # concatenate all file partitions and 
 # append the output to the file output/q1.csv
