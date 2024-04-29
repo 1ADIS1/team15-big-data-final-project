2ed8da654a7fbd23b4c5e3175ef8a5eea6e33403
Ivan Chernakov <ivancholeg@gmail.com>
2024-04-29 18:44:58 +0300

    Updated first hive script

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 source secrets/.psql.pass
 
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f sql/create_partitioned_tables.hql
+
 beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n teamx -p $password -f sql/q1.hql --hiveconf hive.resultset.use.unique.column.names=false > output/q1.csv
