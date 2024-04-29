5288c5417d47e1c75b6c7458c3b0829b06bf912c
Ivan Chernakov <ivancholeg@gmail.com>
2024-04-29 18:42:02 +0300

    Update stage2.sh

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 source secrets/.psql.pass
 
-beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f sql/db.hql > output/hive_results.txt
-
-beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f sql/create_partitioned_tables.hql
-
-beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f sql/q1.hql > output/q1.txt
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n teamx -p $password -f sql/q1.hql --hiveconf hive.resultset.use.unique.column.names=false > output/q1.csv
