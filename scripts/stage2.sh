3ad68a35fd93ca61fded719ced69687f2fab73ed
Ivan Chernakov <ivancholeg@gmail.com>
2024-05-03 18:21:27 +0300

    Update stage2.sh
    Added .sh scripts for 2nd hql query.

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 source secrets/.psql.pass
 
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f sql/create_partitioned_tables.hql
+
+# 1st query
+# Clean outputs
 hdfs dfs -rm -r project/output/q1
 hdfs dfs -rm -r project/hive/warehouse/q1
 
-beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f sql/create_partitioned_tables.hql
-
-# beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f sql/q1.hql --hiveconf hive.resultset.use.unique.column.names=false > output/q1.csv
-
-# beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f sql/q2.hql --hiveconf hive.resultset.use.unique.column.names=false > output/q2.csv
-
-# beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f sql/q3.hql --hiveconf hive.resultset.use.unique.column.names=false > output/q3.csv
-
-
-# Add a header to the output file
 echo "US_state,total_vehicles" > output/q1.csv 
-
 beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f sql/q1.hql
-
-# concatenate all file partitions and 
-# append the output to the file output/q1.csv
 hdfs dfs -cat project/output/q1/* >> output/q1.csv
+
+# 2nd query
+# Clean outputs
+hdfs dfs -rm -r project/output/q2
+hdfs dfs -rm -r project/hive/warehouse/q2
+
+echo "US_state,avg_price" > output/q2.csv 
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f sql/q2.hql
+hdfs dfs -cat project/output/q1/* >> output/q2.csv
