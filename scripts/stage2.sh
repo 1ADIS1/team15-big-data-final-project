160f455e02283341089024c75cb099bbb2678f23
Ivan Chernakov <ivancholeg@gmail.com>
2024-05-03 19:00:31 +0300

    Update stage2.sh
    Added .sh commands for 3rd hql query

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -20,3 +20,12 @@ hdfs dfs -rm -r project/hive/warehouse/q2
 echo "US_state,avg_price" > output/q2.csv 
 beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f sql/q2.hql
 hdfs dfs -cat project/output/q1/* >> output/q2.csv
+
+# 3rd query
+# Clean outputs
+hdfs dfs -rm -r project/output/q3
+hdfs dfs -rm -r project/hive/warehouse/q3
+
+echo "US_state,avg_mileage" > output/q3.csv 
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f sql/q3.hql
+hdfs dfs -cat project/output/q1/* >> output/q3.csv
