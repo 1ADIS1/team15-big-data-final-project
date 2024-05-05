b9d77bbcf7a17d777f7ef32d78f0c7d99030e3aa
Ivan Chernakov <ivancholeg@gmail.com>
2024-05-05 09:20:06 +0300

    Update stage2.sh
    Fixed typo

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -19,7 +19,7 @@ hdfs dfs -rm -r project/hive/warehouse/q2
 
 echo "US_state,avg_price" > output/q2.csv 
 beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f sql/q2.hql
-hdfs dfs -cat project/output/q1/* >> output/q2.csv
+hdfs dfs -cat project/output/q2/* >> output/q2.csv
 
 # 3rd query
 # Clean outputs
@@ -28,4 +28,4 @@ hdfs dfs -rm -r project/hive/warehouse/q3
 
 echo "US_state,avg_mileage" > output/q3.csv 
 beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f sql/q3.hql
-hdfs dfs -cat project/output/q1/* >> output/q3.csv
+hdfs dfs -cat project/output/q3/* >> output/q3.csv
