26d59860de6e80cf4d2f5cd81715f0ca713aa8c3
Ivan Chernakov <ivancholeg@gmail.com>
2024-05-05 10:57:44 +0300

    Added 5th hql query.

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -30,11 +30,20 @@ beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password 
 # beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f sql/q3.hql
 # hdfs dfs -cat project/output/q3/* >> output/q3.csv
 
-# 4th query
-# Clean outputs
-hdfs dfs -rm -r project/output/q4
-hdfs dfs -rm -r project/hive/warehouse/q4
+# # 4th query
+# # Clean outputs
+# hdfs dfs -rm -r project/output/q4
+# hdfs dfs -rm -r project/hive/warehouse/q4
 
-echo "US_state,avg_mileage" > output/q4.csv 
-beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f sql/q4.hql
-hdfs dfs -cat project/output/q4/* >> output/q4.csv
+# echo "US_state,avg_mileage" > output/q4.csv 
+# beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f sql/q4.hql
+# hdfs dfs -cat project/output/q4/* >> output/q4.csv
+
+# 5th query
+# Clean outputs
+hdfs dfs -rm -r project/output/q5
+hdfs dfs -rm -r project/hive/warehouse/q5
+
+echo "US_state,avg_mileage" > output/q5.csv 
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f sql/q5.hql
+hdfs dfs -cat project/output/q5/* >> output/q5.csv
---
+++ sql/q5.hql
@@ -0,0 +1,27 @@
+USE team15_projectdb;
+
+DROP TABLE IF EXISTS q5_results;
+CREATE EXTERNAL TABLE q5_results(
+manufactured_year INT,
+count_years INT
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+LOCATION 'project/hive/warehouse/q5'; 
+
+SET hive.resultset.use.unique.column.names = false;
+
+-- q4
+INSERT INTO q5_results
+SELECT 
+DISTINCT
+manufactured_year,
+COUNT(*) OVER(PARTITION BY manufactured_year) as count_years
+FROM car_vehicles_ext_part_bucket;
+
+INSERT OVERWRITE DIRECTORY 'project/output/q5' 
+ROW FORMAT DELIMITED FIELDS 
+TERMINATED BY ',' 
+SELECT manufactured_year, count_years FROM q5_results
+ORDER BY count_cars DESC
+LIMIT 15;
