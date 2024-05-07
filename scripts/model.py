4c617d1a12f496a880a9a97fad8663ff8da78b3d
Daniel Vakhrushev <45727078+danielpancake@users.noreply.github.com>
2024-05-08 00:57:21 +0300

    Added master yarn

--- scripts/model.py
+++ scripts/model.py
@@ -86,6 +86,7 @@ warehouse = "project/hive/warehouse"
 
 spark = (
     SparkSession.builder.appName(f"{team} - spark ML")
+    .master("yarn")
     .config("hive.metastore.uris", "thrift://hadoop-02.uni.innopolis.ru:9883")
     .config("spark.sql.warehouse.dir", warehouse)
     .config("spark.sql.avro.compression.codec", "snappy")
