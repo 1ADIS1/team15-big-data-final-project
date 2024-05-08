5c0bfec001d3f2f822ede150a3fd4d363d2267cc
Ivan Chernakov <ivancholeg@gmail.com>
2024-05-08 03:15:35 +0300

    Put stage 3 tables to hive

--- scripts/stage3.sh
+++ scripts/stage3.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 source secrets/.psql.pass
+
+hdfs dfs -put -f output/lr_predictions.csv project/output
+hdfs dfs -put -f output/dt_predictions.csv project/output
+hdfs dfs -put -f output/evaluation.csv project/output
+hdfs dfs -put -f output/lr_evaluation.csv project/output
+hdfs dfs -put -f output/dt_evaluation.csv project/output
+hdfs dfs -put -f output/lr_hyperparameters.csv project/output
+hdfs dfs -put -f output/dt_hyperparameters.csv project/output
+hdfs dfs -put -f output/feature_extraction.csv project/output
+
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f sql/create_evaluation_tables.hql
